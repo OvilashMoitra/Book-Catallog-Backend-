b936cce07a12b29ecafe9a565d098a0e54fbe46a
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-09-20 03:49:37 +0600

    route authorization added-2

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -1,11 +1,13 @@
+import { Role } from "@prisma/client";
 import express from "express";
+import { authorization } from "../../middlewares/authorization";
 import { BookController } from "./book.controller";
 
 export const BookRouter = express.Router();
 
-BookRouter.post('/create-book', BookController.createBook)
-BookRouter.delete('/:id', BookController.deleteBook)
+BookRouter.post('/create-book', authorization(Role.admin), BookController.createBook)
+BookRouter.delete('/:id', authorization(Role.admin), BookController.deleteBook)
 BookRouter.get('/:id', BookController.getSingleBook)
 BookRouter.get('/', BookController.getAllBook)
-BookRouter.patch('/:id', BookController.updateBook)
-BookRouter.patch('/:categoryId/category', BookController.updateBook)
+BookRouter.patch('/:id', authorization(Role.admin), BookController.updateBook)
+BookRouter.patch('/:categoryId/category', authorization(Role.admin), BookController.updateBook)
